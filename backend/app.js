686ee3b30eef71de95f2362f7b2845ba2889816e
Jonathan <Jonathan>
2025-03-03 11:23:58 -0600

    Subir proyecto

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+//importar todo de la librería de express
+import express from "express";
+//crear constante que es igual a la libreria que importé y se ejecuta
+const app = express();
+//importo esta constante para usar express en todos lados
+export default app;
---
+++ backend/src/config.js
@@ -0,0 +1,4 @@
+export const config = {
+    PORT: 4000,
+    MONGO_URI: "mongodb://localhost:27017/ZGASDB" //se creará la base si no existe; si usaramos mongoAtlas aquí iría la url de conexión en la nube
+}
